46bb308494a186e8290d1ace5fbc3be45430a5be
Peter van Huisstede <peter.vanhuisstede@eur.nl>
2022-08-28 10:25:24 +0200

    Hiding the large file was NOT a good idea

--- nlp/_modidx.py
+++ nlp/_modidx.py
@@ -5,8 +5,4 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://pvanhuisstede.github.io',
                 'git_url': 'https://github.com/pvanhuisstede/nlp',
                 'lib_path': 'nlp'},
-  'syms': { 'nlp.core': {'nlp.core.foo': ('core.html#foo', 'nlp/core.py')},
-            'nlp.word_embeddings': { 'nlp.word_embeddings.Corpus': ('word_embeddings.html#corpus', 'nlp/word_embeddings.py'),
-                                     'nlp.word_embeddings.Corpus.__init__': ('word_embeddings.html#__init__', 'nlp/word_embeddings.py'),
-                                     'nlp.word_embeddings.Corpus.__iter__': ('word_embeddings.html#__iter__', 'nlp/word_embeddings.py'),
-                                     'nlp.word_embeddings.evaluate': ('word_embeddings.html#evaluate', 'nlp/word_embeddings.py')}}}
+  'syms': {'nlp.word_embeddings': {'nlp.word_embeddings.evaluate': ('word_embeddings.html#evaluate', 'nlp/word_embeddings.py')}}}
